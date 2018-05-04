027a7aecdc55651c5c930c136f977ced3fc8a9a8
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-05-03 22:04:20 -0300

    Add files via upload

--- Templet (modificado)/llamada.h
+++ Templet (modificado)/llamada.h
@@ -21,5 +21,11 @@ int llamada_alta(Llamada* array,int limite);
 int llamada_modificacion(Llamada* array,int limite, int id);
 int llamada_ordenar(Llamada* array,int limite, int orden);
 
+//----------------------------Crear-------------------------------//
+
+int llamada_calcularReclamos(Llamada* array,int limite, int id); // Por cada abonado
+int llamada_cantidadMaximaReclamos(Llamada* array,int limite);
+int informar_imprimirReclamos(Llamada* array,int limite);
+
 
 
